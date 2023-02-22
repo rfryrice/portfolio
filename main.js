5d6ca169e2aec54ce76a678ad102e11cb766c700
Francis Yang <yr.francis2g@gmail.com>
2023-02-21 19:16:06 -0800

    Progress 2-21-23

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es'
 import CannonDebugger from 'cannon-es-debugger';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 import Stats from 'three/addons/libs/stats.module.js';
+
 
 let stats;
 
@@ -14,35 +15,36 @@ const count = 20;
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
-  antialias: true});
-renderer.setPixelRatio( window.devicePixelRatio );
+  antialias: true
+});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Sets the color of the background
 //renderer.setClearColor(0xFEFEFE);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x000 );
+scene.background = new THREE.Color(0x000);
 const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 
-camera.position.set(0,20,45);
-camera.lookAt(0,10,-40);
+camera.position.set(0, 20, 45);
+camera.lookAt(0, 10, -40);
 
 const hemiLight = new THREE.HemisphereLight();
 hemiLight.intensity = 0.35;
-scene.add( hemiLight );
+scene.add(hemiLight);
 
 const dirLight = new THREE.DirectionalLight();
 dirLight.intensity = 0.5;
-dirLight.position.set( 0, 5, 40 );
+dirLight.position.set(0, 5, 40);
 dirLight.castShadow = true;
 dirLight.shadow.camera.zoom = 2;
-scene.add( dirLight );
+scene.add(dirLight);
 
 const meshMaterial = new THREE.MeshLambertMaterial();
 const matrix = new THREE.Matrix4();
@@ -84,13 +86,13 @@ const groundPhysMat = new CANNON.Material();
 const groundDnaContactMat = new CANNON.ContactMaterial(
   groundPhysMat,
   dnaPhysMat,
-  {restitution: 0.9}
+  { restitution: 0.9 }
 );
 
 // scene.add(groundMesh);
 
 const world = new CANNON.World({
-  gravity: new CANNON.Vec3(0,-2,0)
+  gravity: new CANNON.Vec3(0, -2, 0)
 });
 
 const cannonDebugger = new CannonDebugger(scene, world, {
@@ -126,24 +128,24 @@ loader.load('/STL/DNA.STL', function (obj) {
   //console.log(obj)
 
   const defaultTransform = new THREE.Matrix4()
-      .makeRotationX( Math.PI )
-      .multiply( new THREE.Matrix4().makeScale( 0.025, 0.025, 0.025) )
-      .setPosition(-0.5,1,0.5); // This last transform centers inside boxBody
+    .makeRotationX(Math.PI)
+    .multiply(new THREE.Matrix4().makeScale(0.025, 0.025, 0.025))
+    .setPosition(-0.5, 1, 0.5); // This last transform centers inside boxBody
 
-  obj.applyMatrix4( defaultTransform );
+  obj.applyMatrix4(defaultTransform);
 
-  for ( let i = 0; i < count; i++ ) {
-    meshMaterial.color = color.setHex(0x333333 * randomInt(1,3));
-    console.log(meshMaterial.color)
-    matrix.makeScale(1, randomInt(1,2) ,1);
+  for (let i = 0; i < count; i++) {
+    meshMaterial.color = color.setHex(0x333333 * randomInt(1, 3));
+    // console.log(meshMaterial.color)
+    matrix.makeScale(1, randomInt(1, 2), 1);
 
     dna = new THREE.Mesh(obj, meshMaterial);
-    dna.applyMatrix4( matrix );
+    dna.applyMatrix4(matrix);
 
     dnabox = new CANNON.Body({
       mass: 1,
-      shape: new CANNON.Box(new CANNON.Vec3(0.4,1,0.4)),
-      position: new CANNON.Vec3((i*2)-15, randomInt(5, 30), randomInt(-50,30)),
+      shape: new CANNON.Box(new CANNON.Vec3(0.4, 1, 0.4)),
+      position: new CANNON.Vec3((i * 2) - 15, randomInt(5, 30), randomInt(-50, 30)),
       linearDamping: 0.8,
       material: dnaPhysMat
     });
@@ -162,7 +164,7 @@ loader.load('/STL/DNA.STL', function (obj) {
 // triggerBody.addEventListener('collide', (event) => {
 //   if (event.body) {
 //     console.log('The dna entered the trigger!', event)
-    
+
 //   }
 // })
 // world.addEventListener('endContact', (event) => {
@@ -177,7 +179,7 @@ loader.load('/STL/DNA.STL', function (obj) {
 const timeStep = 1 / 60;
 
 stats = new Stats();
-document.body.appendChild( stats.dom );
+document.body.appendChild(stats.dom);
 
 // console.log(meshes)
 // console.log(multiboxBodies)
@@ -186,17 +188,17 @@ function animate() {
   groundMesh.position.copy(groundBody.position);
   groundMesh.quaternion.copy(groundBody.quaternion);
 
-  for (let i = 0; i < count; i++){
-    multiboxBodies[i].angularVelocity.set(0,randomInt(0,3),0)
+  for (let i = 0; i < count; i++) {
+    multiboxBodies[i].angularVelocity.set(0, randomInt(0, 3), 0)
     //console.log(multiboxBodies[10].position)
 
     if (multiboxBodies[i].position['y'] <= 3) {
       // matrix.makeScale( 1, Math.random(), 1 )
       // meshes[i].applyMatrix4(matrix);
-      multiboxBodies[i].position['y'] = randomInt(3,50);
+      multiboxBodies[i].position['y'] = randomInt(3, 50);
       meshes[i].position.copy(multiboxBodies[i].position);
       meshes[i].quaternion.copy(multiboxBodies[i].quaternion);
-    } else{
+    } else {
       meshes[i].position.copy(multiboxBodies[i].position);
       meshes[i].quaternion.copy(multiboxBodies[i].quaternion);
     }
@@ -204,21 +206,72 @@ function animate() {
 
   world.fixedStep();
   // cannonDebugger.update();
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
   render();
-  
+
   stats.update();
 }
 
 function render() {
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 
 }
 
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// --End of Background Animation--
+
+const stl_viewer = new StlViewer(document.getElementById("stl_cont"),
+  {
+    allow_drag_and_drop: false,
+    auto_resize: true,
+    models: new Array(
+      { id: 0, filename: "../STL/DuckGear.stl", rotationx: 1 },
+      { id: 1, filename: "../STL/EEGboard_housing_v3.STL", rotationx: 1, opacity: 0 },
+      { id: 2, filename: "../STL/SampleLeafEmboss.STL", rotationx: 1, opacity: 0 },
+      { id: 3, filename: "../STL/FilmHolder_Bottom.STL", x: -70, rotationx: 1, opacity: 0 },
+      { id: 4, filename: "../STL/FilmHolder_Top.STL", x: 70, rotationx: 1, opacity: 0 }
+    ),
+
+  });
+
+let modelData;
+const fileURL = '../model_descriptions.json';
+
+fetch(fileURL)
+  .then(response => response.json())
+  .then(data => {
+    modelData = data;
+  })
+  .catch(error => {
+    console.error('Error loading JSON file:', error);
+  });
+
+const selectElement = document.querySelector('#models');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('#project-text');
+  const getID = event.target.value;
+  result.textContent = modelData[getID]['p'];
+  if (getID == 'film') {
+    for (let i = 0; i < 3; i++) {
+      stl_viewer.set_opacity(i, 0)
+    }
+    stl_viewer.set_opacity(3, 1);
+    stl_viewer.set_opacity(4, 1);
+  } else {
+    for (let i = 0; i < 5; i++) {
+      stl_viewer.set_opacity(i, 0)
+    }
+    stl_viewer.set_opacity(modelData[getID]['id'], 1)
+
+  }
+
+
 });
